4cbede5d90616cf87f952ec93a534e05c1cab926
Cody Pizzaia <cpizzaia@asu.edu>
2016-02-16 11:12:46 -0500

    initial commit

---
+++ webpack.config.js
@@ -0,0 +1,32 @@
+var path = require("path");
+
+module.exports = {
+  context: __dirname,
+  entry: "./frontend/entry.js",
+  output: {
+    path: path.join(__dirname, 'app', 'assets', 'javascripts'),
+    filename: "bundle.js",
+    devtoolModuleFilenameTemplate: '[resourcePath]',
+    devtoolFallbackModuleFilenameTemplate: '[resourcePath]?[hash]'
+  },
+  devtool: 'source-maps',
+  resolve: {
+    extensions: ["", ".js"]
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.jsx?$/,
+        exclude: /(node_modules|bower_components)/,
+        loader: 'babel',
+        query: {
+          presets: ['react', 'es2015']
+        }
+      },
+      {
+        test: /\.node$/,
+        loader: "node-loader"
+      }
+    ]
+  }
+};
